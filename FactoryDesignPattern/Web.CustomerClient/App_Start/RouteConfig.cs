409c7c3ad4b6e1ed672c4d7f0006b8eabd17bb20
Sayantan Choudhury <say.m146@hotmail.com>
2019-09-08 19:30:44 +0530

    Route config modified

--- FactoryDesignPattern/Web.CustomerClient/App_Start/RouteConfig.cs
+++ FactoryDesignPattern/Web.CustomerClient/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace Web.CustomerClient
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Customer", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
