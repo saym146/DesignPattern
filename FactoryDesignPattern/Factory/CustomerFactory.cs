ab3c6fb96c497753c18c3b019a554885e409eb7d
Sayantan Choudhury <say.m146@hotmail.com>
2019-09-08 19:25:12 +0530

    Factory Design pattern project added

---
+++ FactoryDesignPattern/Factory/CustomerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Model;
+using ModelContract;
+
+namespace Factory
+{
+    public class CustomerFactory //Design Pattern : Simple Factory
+    {
+        private static Dictionary<string, ICustomer> _CustomerTypes = null; // Design Pattern : RIP principle
+        public static ICustomer GetInstance(string type)
+        {
+            if (_CustomerTypes == null)
+            {
+                _CustomerTypes = new Dictionary<string, ICustomer>();
+                _CustomerTypes.Add(key: "C", value: new Customer());
+                _CustomerTypes.Add(key: "L", value: new Lead());
+            }
+            return _CustomerTypes[type];
+        }
+    }
+}
---
+++ FactoryDesignPattern/Model/CustomerModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ModelContract;
+
+namespace Model
+{
+    public abstract class CustomerBase : ICustomer
+    {
+        public string CustomerType { get; set; }
+        public string CustomerName { get; set; }
+        public double BillAmount { get; set; }
+        public string Dob { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+
+        public virtual bool Validate()
+        {
+            throw new Exception("Method not available");
+        }
+    }
+    public class Customer : CustomerBase
+    {
+        public override bool Validate()
+        {
+            if (String.IsNullOrEmpty(this.CustomerName))
+            {
+                throw new Exception("Customer Name is required");
+            }
+            else if (DateTime.Now.Year - Convert.ToDateTime(this.Dob).Year <= 3)
+            {
+                throw new Exception("Please enter a valid Dob");
+            }
+            else if (this.BillAmount <=0)
+            {
+                throw new Exception("Bill Amount is required");
+            }
+            else if (String.IsNullOrEmpty(this.Address))
+            {
+                throw new Exception("Customer Address is required");
+            }
+            else if (String.IsNullOrEmpty(this.Contact))
+            {
+                throw new Exception("Customer Contact is required");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+    public class Lead : CustomerBase
+    {
+        public override bool Validate()
+        {
+            if (String.IsNullOrEmpty(this.CustomerName))
+            {
+                throw new Exception("Customer Name is required");
+            }
+            else if (String.IsNullOrEmpty(this.Contact))
+            {
+                throw new Exception("Customer Contact is required");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
